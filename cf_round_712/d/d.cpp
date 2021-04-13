119346878d19a168c6d37db64c19b56a292c0cbd
Pradip Khakurel <pradip.khakurel@gmail.com>
2021-04-13 22:07:33 +0200

    edit cf round 712

--- cf_round_712/d/d.cpp
+++ cf_round_712/d/d.cpp
@@ -10,95 +10,71 @@
 
 using namespace std;
 
-struct ColorInfo {
-    int color;
-    int r;
-    int c;
-
-    ColorInfo(int _color, int _r, int _c) {
-        color = _color;
-        r = _r;
-        c = _c;
-    }
-};
-
-struct Cell {
-    int r;
-    int c;
-
-    Cell(int _r, int _c) {
-        r = _r;
-        c = _c;
-    }
-};
-
 int NO_COLOR = 0;
 int BLACK = 1;
 int WHITE = 2;
 int RED = 3;
 
-queue<Cell> blacks;
-queue<Cell> whites;
+struct Cell {
+    int r;
+    int c;
+    int color;
 
-bool isOk(int r, int c, int n) {
-    return r >= 0 && r < n && c >= 0 && c < n;
-}
+    Cell(int _r, int _c) {
+        r = _r;
+        c = _c;
+        color = NO_COLOR;
+    }
+};
 
-Cell colorOneCellDiagonally(vector<vector<int>> &grid, queue<Cell> &cells, int color) {
+Cell colorOneCell(queue<Cell> &cells, int color) {
     Cell curCell = cells.front();
     cells.pop();
-    grid[curCell.r][curCell.c] = color;
+    curCell.color = color;
 
     return curCell;
 }
 
-ColorInfo getNextColor(vector<vector<int>> &grid, int forbiddenColor) {
+Cell getNextColoredCell(queue<Cell> &blackCells, queue<Cell> &whiteCells, int forbiddenColor) {
     int color;
-    Cell cell(-1, -1);
 
-    if(blacks.size() == 0) {
+    if(blackCells.size() == 0) {
         color = forbiddenColor != WHITE ? WHITE : RED; 
-        cell = colorOneCellDiagonally(grid, whites, color);
+        return colorOneCell(whiteCells, color);
     }
-    else if(whites.size() == 0) {
+    else if(whiteCells.size() == 0) {
         color = forbiddenColor != BLACK ? BLACK : RED;
-        cell = colorOneCellDiagonally(grid, blacks, color);
+        return colorOneCell(blackCells, color);
     }
     else if(forbiddenColor != BLACK) {
-        color = BLACK;
-        cell = colorOneCellDiagonally(grid, blacks, color);
+        return colorOneCell(blackCells, BLACK);
     }
     else {
-        color = WHITE;
-        cell = colorOneCellDiagonally(grid, whites, color);
+        return colorOneCell(whiteCells, WHITE);
     }
-
-    return ColorInfo(color, cell.r, cell.c);
 }
 
 int main() {
     int n, c;
+    queue<Cell> blackCells;
+    queue<Cell> whiteCells;
 
     cin >> n;
-
-    vector<vector<int>> grid(n, vector<int>(n));
 
     for(int i=0;i<n;i++) {
         for(int j=0;j<n;j++) {
             if((i+j)%2 == 0) {
-                blacks.push(Cell(i, j));
+                blackCells.push(Cell(i, j));
             }
             else {
-                whites.push(Cell(i, j));
+                whiteCells.push(Cell(i, j));
             }
         }
     }
 
     for(int i=0;i<n*n;i++) {
         cin >> c;
-        ColorInfo info = getNextColor(grid, c);
-
-        cout << info.color << " " << info.r+1 << " " << info.c+1 << "\n";
-        //cout.flush();
+        Cell cell = getNextColoredCell(blackCells, whiteCells, c);
+        cout << cell.color << " " << cell.r+1 << " " << cell.c+1 << "\n";
     }    
 } 
